ec416f9f86569934303d590c4b4e4eb13e551f30
abdul mohsin <mohsin050595@gmail.com>
2024-01-13 20:11:59 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [text,setText] = useState("")
+  // const [text,setText] = useState("")
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://demo-ashy-phi.vercel.app/")
-      const data = await res.json()
-      console.log(data)
-      setText(data)
-    }
-    fetchData()
-  },[])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("https://demo-ashy-phi.vercel.app/")
+  //     const data = await res.json()
+  //     console.log(data)
+  //     setText(data)
+  //   }
+  //   fetchData()
+  // },[])
   return (
     <>
       <h1>{text}</h1>
+      <h1>Hello world</h1>
     </>
   )
 }
